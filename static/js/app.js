1061c05c162e8164923caf00e535325c4ed0f03a
Kenway Tennant <kengtennant@gmail.com>
2024-04-13 22:24:28 -0700

    Started leaflet plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,23 @@
-// Read CSV file 
-d3.csv("Data/LA_Crime_Data.csv").then(function(data) {
-    console.log(data);
-  });
+// Hold the data from JSON/CSV file
+let allData;
 
+// Create initial map object using Los Angeles coordinates
+let myMap = L.map("map", {
+    center: [34.05, -118.24],
+    zoom: 10
+});
+
+// Add tiles into LA_Map object
+// Use topical, not street view
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Read CSV file  
+d3.csv("Data/LA_Crime_Data.csv").then(function(data) {
+    allData = data;
+    console.log("allData", allData);
+});
+
+Get_LA_Boundary_Data();
